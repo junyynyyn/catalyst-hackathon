3e27eb538b3b8e1646882ff875bc4b69b3225499
lkchue <lachlan.chue@student.unimelb.edu.au>
2024-08-18 15:59:41 +1000

    added translate up animation and styling to game item

--- catalyst-app/app/components/game_item.tsx
+++ catalyst-app/app/components/game_item.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const GameItem = (item: any) => {
-    return (
-      <div className="game-item">
-        <img className="game-item-picture" src={item.item.picture}/>
-        <Link href={"/item/" + item.item.id}>
+  return (
+    <div className="game-item">
+      <Link href={"/item/" + item.item.id}>
+        <img className="game-item-picture" src={item.item.picture} />
         <div className="game-item-content">
-            <div className="game-item-name">{item.item.item_name}</div>
-            <div className="game-item-price">{item.item.price}</div>
-            <div className="game-item-desc">{item.item.info}</div>
+          <div className="game-item-name">{item.item.item_name}</div>
+          <div className="game-item-price">{item.item.price}</div>
+          <div className="game-item-desc">{item.item.info}</div>
         </div>
-        </Link>
-      </div>
-  )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default GameItem
+export default GameItem;
