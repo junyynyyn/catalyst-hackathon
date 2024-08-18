5974cdb2dbaf4ab29aae2c3d69bb6c861740b20b
lkchue <lachlan.chue@student.unimelb.edu.au>
2024-08-18 11:43:35 +1000

    created organisation buttons

--- catalyst-app/app/league/page.tsx
+++ catalyst-app/app/league/page.tsx
@@ -1,29 +1,32 @@
-import { get_prices } from '../price_utils';
-import { get_items } from '../item_utils';
+import { get_prices } from "../price_utils";
+import { get_items } from "../item_utils";
 
 export default async function LeagueHome() {
   const items = await get_items("league");
   const items_list = items.map((item: any) => (
     <div className="game-item">
-      <img src={item.picture}/>
+      <img src={item.picture} />
       <div className="game-item-content">
-          <p>
-              <div className="game-item-name">{item.item_name}</div>
-              <div className="game-item-price">{item.price}</div>
-              <div className="game-item-desc">Boiler Text</div>
-          </p>
+        <p>
+          <div className="game-item-name">{item.item_name}</div>
+          <div className="game-item-price">{item.price}</div>
+          <div className="game-item-desc">Boiler Text</div>
+        </p>
       </div>
-  </div>
+    </div>
   ));
   return (
     <div>
       <div className="game-list-header">
-          <h2 className="game-title">League of Legends</h2>
+        <h2 className="game-title">League of Legends</h2>
+        <div className="organise-opt-container">
+          <div className="organise-opt">Default</div>
+          <div className="organise-opt">A-Z</div>
+          <div className="organise-opt">List view</div>
+        </div>
       </div>
 
-      <div className="game-list">
-          {items_list}
-      </div>
-  </div>
+      <div className="game-list">{items_list}</div>
+    </div>
   );
 }
